4517a2f469a56bed70c465e5664201f3a0d91672
Ramy Gendy <ramygendy90@gmail.com>
2023-05-20 23:24:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ model= joblib.load('final_model_1.sav')
 
 st.title('How your restaurant will success in Bangalore?')
 
-online_order= st.selectbox('online order option in the resturant?',['Yes','No'])
-book_table= st.selectbox('Book table option in th resturant?',['Yes','No'])
-votes= st.slider('how many votes the resturant have?', min_value=0, max_value=16832, value=0, step=5)
-average_cost= st.number_input('how much on average you want a two persons to spend?',min_value=50, max_value=6000, value=50, step=50)
-meal_type= st.selectbox('what type of the resturant',('Buffet','Cafes','Delivery','Desserts',
-                                                      'Bars','Drinks & nightlife','Dine_out'))
+online_order= st.selectbox('online order option in the resturant?',['Yes', 'No'])
+book_table= st.selectbox('Book table option in th resturant?',['Yes', 'No'])
+votes= st.slider('how many votes the resturant have?',
+                 min_value=0, max_value=16832, value=0, step=50)
+average_cost= st.slider('how much on average you want a two persons to spend?',
+                        min_value=50, max_value=6000, value=50, step=50)
+meal_type= st.selectbox('what type of the resturant', 
+                        ['Buffet', 'Cafes', 'Delivery', 'Desserts', 'Dine-out', 'Drinks & nightlife', 'Pubs and bars'])
 neighborhood= st.selectbox('Where you will start your restaurant?',
                            ['Banashankari', 'Bannerghatta Road', 'Basavanagudi', 'Bellandur',
                             'Brigade Road', 'Brookefield', 'BTM', 'Church Street',
@@ -23,16 +25,15 @@ neighborhood= st.selectbox('Where you will start your restaurant?',
                             'Malleshwaram', 'Marathahalli', 'MG Road', 'New BEL Road',
                             'Old Airport Road', 'Rajajinagar', 'Residency Road',
                             'Sarjapur Road', 'Whitefield'])
-rest_type_count= st.select_slider('how many area the resturant serveing',[1,2])
-cuisines_total= st.select_slider('how many cuisines the resturant serveing',[1,2,3,4,5,6,7,8])
+rest_type_count= st.select_slider('how many area the resturant serveing',[1, 2])
+cuisines_total= st.select_slider('how many cuisines the resturant serveing',[3, 2, 1, 4, 5, 8, 7, 6])
 
 
-columns= ['online_order', 'book_table', 'votes', 'average_cost', 'meal_type',
-          'neighborhood', 'rest_type_count', 'cuisines_total']
+columns= ['online_order', 'book_table', 'votes', 'average_cost', 'meal_type','neighborhood', 'rest_type_count', 'cuisines_total']
 
 
 def predict():
-    col= np.array(['online_order', 'book_table', 'votes', 'average_cost', 'meal_type','neighborhood', 'rest_type_count', 'cuisines_total'])
+    col= np.array([online_order, book_table, votes, average_cost, meal_type, neighborhood, rest_type_count, cuisines_total])
     data= pd.DataFrame([col], columns=columns)
     prediction= model.predict(data)[0]
 
